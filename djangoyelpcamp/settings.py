3e3de939367569c2d104f4f054bae734c10fa3b1
Momo Johnson <mo12g13@gmail.com>
2018-07-05 20:35:35 +0000

    Setup email configurations in settings.py file

--- djangoyelpcamp/settings.py
+++ djangoyelpcamp/settings.py
@@ -127,6 +127,11 @@ EMAIL_SUBJECT_PREFIX = '[Yelp Camp Team]'
 
 
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
